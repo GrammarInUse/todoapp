8e495a1a88be48d38f3830fe8f693384f4d6df0b
thelimit1998 <thelimit1998@users.noreply.github.com>
2020-05-17 17:50:56 +0700

    upload hinh, restart database

--- test.js
+++
@@ -1,8 +0,0 @@
-const Photos = require("./services/photos").photos;
-
-async function tempFunction(){
-    var k = await Photos.getLostId(1);
-    console.log("KET QUA LA: ", k[0]);
-}
-
-tempFunction();
